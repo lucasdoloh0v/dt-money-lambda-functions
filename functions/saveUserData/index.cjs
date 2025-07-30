44624f42a5ad8a0f8e4ffc524e607b893e90738b
Lucas Alves <lucasdolohov@gmail.com>
2025-07-30 04:08:55 -0300

    fix  imports

--- functions/saveUserData/index.cjs
+++ functions/saveUserData/index.cjs
@@ -1,4 +1,4 @@
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
 
 const client = new DynamoDBClient({ region: 'us-east-2' });
 
